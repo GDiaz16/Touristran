2bd50a9e91aa2190fc324d2e459ab4f723376602
Gonzalo Diaz <egonzalodm@gmail.com>
2020-08-13 13:23:00 -0500

    entidades js

---
+++ FrontendVUE/touristran/src/Entities/TouristicEntity.js
@@ -0,0 +1,120 @@
+class TouristEntity {
+  constructor(
+    pkTourist,
+    fkCity,
+    name,
+    birthday,
+    idOfTourist,
+    idType,
+    travelFrequencyInMonths,
+    budget,
+    hasCreditCard
+  ) {
+    this.pkTourist = pkTourist;
+    this.fkCity = fkCity;
+    this.name = name;
+    this.birthday = birthday;
+    this.idOfTourist = idOfTourist;
+    this.idType = idType;
+    this.travelFrequencyInMonths = travelFrequencyInMonths;
+    this.budget = budget;
+    this.hasCreditCard = hasCreditCard;
+  }
+
+  getPkTourist() {
+    return this.pkTourist;
+  }
+
+  setPkTourist(pkTourist) {
+    this.pkTourist = pkTourist;
+  }
+
+  getFkCity() {
+    return this.fkCity;
+  }
+
+  setFkCity(fkCity) {
+    this.fkCity = fkCity;
+  }
+
+  getName() {
+    return name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getBirthday() {
+    return this.birthday;
+  }
+
+  setBirthday(birthday) {
+    this.birthday = birthday;
+  }
+
+  getIdType() {
+    return this.idType;
+  }
+
+  setIdType(idType) {
+    this.idType = idType;
+  }
+
+  getTravelFrequencyInMonths() {
+    return this.travelFrequencyInMonths;
+  }
+
+  setTravelFrequencyInMonths(travelFrequencyInMonths) {
+    this.travelFrequencyInMonths = travelFrequencyInMonths;
+  }
+
+  getBudget() {
+    return this.budget;
+  }
+
+  setBudget(budget) {
+    this.budget = budget;
+  }
+
+  getHasCreditCard() {
+    return this.hasCreditCard;
+  }
+
+  setHasCreditCard(hasCreditCard) {
+    this.hasCreditCard = hasCreditCard;
+  }
+
+  getIdOfTourist() {
+    return this.idOfTourist;
+  }
+
+  setIdOfTourist(idOfTourist) {
+    this.idOfTourist = idOfTourist;
+  }
+
+  toString() {
+    return (
+      "TouristEntity{" +
+      "pkTourist=" +
+      this.pkTourist +
+      ", fkCity=" +
+      this.fkCity +
+      ", name=" +
+      this.name +
+      ", birthday=" +
+      this.birthday +
+      ", id=" +
+      this.idOfTourist +
+      ", idType=" +
+      this.idType +
+      ", travelFrequencyInMonths=" +
+      this.travelFrequencyInMonths +
+      ", budget=" +
+      this.budget +
+      ", hasCreditCard=" +
+      this.hasCreditCard +
+      "}"
+    );
+  }
+}
